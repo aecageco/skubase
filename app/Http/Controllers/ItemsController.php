c5f4eb90b0ccb434f1ab3a62f49fb111868b4f7c
brianae <bjones@aecageco.com>
2025-10-24 13:40:21 -0400

    test

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Items;
 use App\Models\ProductMeta;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Response;
 
 class ItemsController extends Controller
@@ -82,20 +81,8 @@ class ItemsController extends Controller
     public function index()
 
     {                $type="All";
-
-        $all = \App\Models\ProductMeta::on('sqlite2')->pluck('sku')->all();
-        $chunks = array_chunk($all, 800);
-
-        $base = \App\Models\Items::whereIn('sku', $chunks[0]);
-        foreach (array_slice($chunks, 1) as $c) {
-            $base->unionAll(\App\Models\Items::whereIn('sku', $c));
-        }
-
-        $items = DB::query()
-            ->fromSub($base, 'u')
-            ->orderBy('id')
-            ->paginate(25);
-
+        $skus  = \App\Models\ProductMeta::distinct('sku')->pluck('sku','sku');
+        $items = \App\Models\Items::whereIn('sku', $skus)->paginate(25);
         return view('items.index', compact('items','type'));
     }
 
